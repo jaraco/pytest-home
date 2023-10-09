fa643dc3fba105309cbb9ff52d58863cfaa75e0c
Jason R. Coombs <jaraco@jaraco.com>
2023-10-09 15:36:24 -0400

    Expand docstring with a narrative about what's happening.

--- pytest_home/fixtures.py
+++ pytest_home/fixtures.py
@@ -7,12 +7,33 @@ import pytest
 @pytest.fixture
 def tmp_home_dir(monkeypatch, tmp_path_factory):
     """
+    A temporary home directory fixture.
+
+    Configures the home directory to a temporary directory,
+    hiding the user's dotfiles and other home-bound state.
+
+    Before the fixture is enacted, home resolves to the user's
+    usual home dir.
+
+    >>> import pathlib
+    >>> orig = pathlib.Path('~').expanduser()
+
+    When the fixture is triggered, it alters the home directory,
+    returning the altered directory.
+
     >>> home = getfixture('tmp_home_dir')
+
+    Home is empty by default (feel free to inject state as needed).
+
     >>> list(home.iterdir())
     []
-    >>> import pathlib
+
+    Now ``~`` expands to the temporary home.
+
     >>> pathlib.Path('~').expanduser() == home
     True
+    >>> pathlib.Path('~').expanduser() == orig
+    False
     """
     return _set(monkeypatch, tmp_path_factory.mktemp('home'))
 
